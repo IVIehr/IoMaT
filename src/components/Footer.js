ed5a5511b615a8d65a6cf4ad77666ee14b7d5eef
mehrnoush.navidi <mehr910093@gmail.com>
2023-02-09 16:11:30 +0330

    Implement the hompage and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
+              <ul className="footer_nav">
+                <li>
+                  <a href="#">تماس با ما</a>
+                </li>
+                <li>
+                  <a href="#">همکاری</a>
+                </li>
+                <li>
+                  <a href="#">مجله فرادرس</a>
+                </li>
+                <li>
+                  <a href="#">پیشنهاد آموزش</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
+              <ul>
+                <li>
+                  <a href="#">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fab fa-twitter"></i>{" "}
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fab fa-instagram"></i>{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a href="#">
+                    <i className="fab fa-linkedin-in"></i>{" "}
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="footer_nav_container">
+              <div className="cr">
+              © فرادرس 1401. تمامی حقوق محفوظ است.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Footer from "./Footer";
+import Products from "./Products";
+
+function Home() {
+  return (
+    <div className="">
+      <div className="hero">
+        <div className="card bg-dark text-white border-0 ">
+          <img
+            src="/assets/shopping-bg.jpeg"
+            className="card-img"
+            alt="Background"
+            height="700px"
+          />
+          <div className="card-img-overlay d-flex hero-header flex-column justify-content-center">
+            <div className="container">
+              <h5 className="card-title display-3 fw-bolder mb-0">
+                NEW SEASON ARRIVALS
+              </h5>
+              <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Products />
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
