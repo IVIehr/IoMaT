30de0c7bf86196633077d1e7ee1d57ebf5a2e236
mehrnoush.navidi <mehr910093@gmail.com>
2023-02-09 21:49:32 +0330

    Complete App.js & index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Product from "./components/Product";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import Teaching from "./components/Teaching";
+import Cooperation from "./components/Cooperation";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+const promise = loadStripe(
+  "pk_test_51IdVudBDYzH08iN6B3r4f8sFZke2NPuHtA2wRkqsvql1cKJCHuv5X4lQcGdpvCMBfz5JMirO3Q2uRvaF2Md4OL2G00we93HQ5l"
+);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/checkout"
+            element={
+              <Elements stripe={promise}>
+                <Checkout />
+              </Elements>
+            }
+          />
+          <Route path="/teaching" element={<Teaching />} />
+          <Route path="/cooperation" element={<Cooperation />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
