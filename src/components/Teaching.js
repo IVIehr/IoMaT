2ea319375f410b751fb251d2bd61f47d89806743
mehrnoush.navidi <mehr910093@gmail.com>
2023-02-09 16:46:25 +0330

    Add Teaching component

---
+++ src/components/Teaching.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function Teaching() {
+  const userState = useSelector((userState) => userState.handleUser);
+  const navigate = useNavigate();
+
+  const proceedTeaching = () => {
+    if (userState !== null) {
+      navigate("/contact");
+    } else {
+      toast("لطفا ابتدا وارد شوید");
+    }
+  };
+  return (
+    <div>
+      <div className="container py-5 my-5">
+        <div className="row">
+        <div className="col-md-6 d-flex justify-content-center">
+            <img
+              src="assets/about.png"
+              alt="About Us"
+              height="400px"
+              width="400px"
+            />
+          </div>
+          <div className="col-md-6">
+            <h1 className="fw-bold mb-4 dark-text">فرادرس</h1>
+            <h5>تدریس و عضویت در هیات علمی بزرگترین دانشگاه کشور بر بستر وب</h5>
+            <p className="lead mb-4">
+            تدریس در فرادرس به دانش و تخصص شما دایره اثری به وسعت کل کشور می‌بخشد. دانش و تجربه‌ای که تاکنون محدود به زمان و جغرافیا بود، در فرادرس اوج گرفته و فارغ از هر گونه مرزی می‌شود. تدریس در فرادرس، تدریس برای آرمان «دانش بدون مرز» است.
+            </p>
+            <button
+            onClick={proceedTeaching}
+            className="btn btn-dark mb-5 mx-auto"
+          >
+            انتشار آموزش را شروع کنید
+          </button>
+          <ToastContainer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Teaching;
