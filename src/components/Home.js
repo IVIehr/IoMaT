5606426850ce3ed7d59bb21793cde59aedf39f09
mehrnoush.navidi <mehr910093@gmail.com>
2023-02-10 11:22:43 +0330

    Enhance the search box result

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,9 @@
-import { useState, useEffect, React} from "react";
-import { useNavigate } from "react-router-dom";
+import {React} from "react";
 import Offer from "./Offer";
 import Products from "./Products";
 import SearchBox from "./searchBox";
 
 function Home() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [data, setData] = useState([]);
-  const navigate = useNavigate();
-  let componentMouted = true;
-
-  useEffect(() => {
-    const getProducts = async () => {
-      const response = await fetch("https://fakestoreapi.com/products");
-      if (componentMouted === true) {
-        setData(await response.clone().json());
-      }
-      return () => {
-        componentMouted = false;
-      };
-    };
-    getProducts();
-  }, []);
-
-  const handleSearch = (query) =>{
-    setSearchQuery(query);
-  }
-
-  const handleSubmit = (e) =>{
-    e.preventDefault();
-    var filtered;
-    if(searchQuery){
-      filtered = data.filter(m => m.title.toLowerCase().includes(searchQuery.toLowerCase()));
-    }
-    if(filtered.length != 0){
-      var id = filtered[0].id
-      navigate(`/products/${id}`)
-    }
-    else{
-      console.log('no');
-    }
-  }
-
   return (
     <div className="">
       <div className="hero">
@@ -52,12 +14,12 @@ function Home() {
             alt="Background"
             height="400px"
           />
-          <div className="card-img-overlay d-flex hero-header flex-column align-items-center justify-content-center">
+          <div className="card-img-overlay d-flex hero-header mt-5 flex-column align-items-center justify-content-center">
             <div className="container text-center">
               <p className="card-text lead fs-5 fw-bold">در بین بیش از ۲۰,۰۰۰ ساعت آموزش فرادرسی جستجو کنید و به جمع میلیونی دانشجویان فرادرس بپیوندید.</p>
             </div>
-            <div className="w-25 mt-4">
-              <SearchBox value={searchQuery} onchange={handleSearch} onsubmit={handleSubmit}/>
+            <div className="mt-5">
+              <SearchBox/>
             </div>
           </div>
         </div>
--- src/components/searchBox.js
+++ src/components/searchBox.js
@@ -1,22 +1,64 @@
-import React from 'react';
+import { useState, useEffect, React} from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import SearchResult from "./SearchResult";
 
-const SearchBox = ({value, onchange, onsubmit}) => {
+const SearchBox = () => {
+    const [value, setValue] = useState("");
+    const [data, setData] = useState([]);
+    const navigate = useNavigate();
+    let componentMouted = true;
+
+    useEffect(() => {
+        const getProducts = async () => {
+          const response = await fetch("https://fakestoreapi.com/products");
+          if (componentMouted === true) {
+            setData(await response.clone().json());
+          }
+          return () => {
+            componentMouted = false;
+          };
+        };
+        getProducts();
+      }, []);
+
+    const proceedTeaching = (e) => {
+        e.preventDefault();
+        var filtered;
+        if(value){
+          filtered = data.filter(m => m.title.toLowerCase().includes(value.toLowerCase()));
+        }
+        if(filtered.length != 0){
+          var id = filtered[0].id
+          navigate(`/products/${id}`)
+            // <SearchResult data={filtered}/>
+            // navigate('/result')
+        }
+        else{
+          toast("آموزش مورد نظر یافت نشد");
+        }
+      };
+    
+      const handleSearch = (query) =>{
+        setValue(query);
+      }
     return ( 
-        <form class="input-group mb-3" onSubmit={onsubmit}>
+        <form class="input-group mb-3">
             <input 
                 type="text"
-                value={value}
                 className="form-control"
                 placeholder='جستجوی آموزش...'
                 aria-label="Recipient's username"
-                onChange={e => onchange(e.currentTarget.value)}
+                onChange={e => handleSearch(e.currentTarget.value)}
                 aria-describedby="button-addon2"/>
             <button 
                 className="btn btn-success"
                 type="submit"
                 id="button-addon2"
+                onClick={e => proceedTeaching(e)}
                 >جستجو
             </button>
+            <ToastContainer />
         </form>
      );
 }
