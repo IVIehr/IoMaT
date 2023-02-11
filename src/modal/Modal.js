f353cb00015e30ec2f175842fb4c335e921d03b1
mehrnoush.navidi <mehr910093@gmail.com>
2023-02-12 00:21:20 +0330

    Complete the terms & conditions modal

--- src/modal/Modal.js
+++ src/modal/Modal.js
@@ -1,14 +1,20 @@
 import './modal.css';
 
 function Modal({ handleClose, show}){
-  const showHideClassName = show ? "modal display-block" : "modal display-none";
+  const showHideClassName = show ? "modal d-block" : "modal d-none";
 
   return (
     <div className={showHideClassName}>
-      <section className="modal-main">
-        hi
-        <button type="button" onClick={handleClose}>
-          Close
+      <section className="modal-main p-4 rounded-3 w-50">
+        <h5 className='text-center text-info'>شرایط و ضوابط</h5>
+        <ul className='mt-4 text-end'>
+          <li className='my-1'>خرید کالا از وب‌سایت فروشگاه فرادرس بر مبنای قوانین و آئین‌نامه‌های تجارت الکترونیک و با رعایت کامل تمام قوانین جمهوری اسلامی ایران انجام می‌شود.</li>
+          <li className='my-1'>كاربران متعهد هستند که در دسترسی به وب‌سایت فروشگاه فرادرس مقاصدی خارج از دریافت اطلاعات فنی یا سفارش اینترنتی کالا دنبال نکنند.</li>
+          <li className='my-1'>كاربران هنگام سفارش كالا ملزم هستند اطلاعات صحیح و كامل خود را در وب‌سایت درج کنند. بدیهی است کاستی یا نادرستی اطلاعات مانع تکمیل درست سفارش و تحویل به‌موقع آن خواهد شد.</li>
+          <li className='my-1'>فروشگاه فرادرس هیچ‌گونه مسئولیتی در قبال محتویات سایت‌هایی که ارتباط به آن‌ها به واسطه‌ی آگاهی و یا لینک در وب‌سایت فرادرس شکل می‌گیرد، ندارد.</li>
+        </ul>
+        <button className='btn btn-outline-info mt-4' type="button" onClick={handleClose}>
+          برگشت
         </button>
       </section>
     </div>
