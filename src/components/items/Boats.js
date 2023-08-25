513f51b1527c0cabe34287bb905c5acaff97d034
mehrnoush.navidi <mehr910093@gmail.com>
2023-08-25 13:42:21 +0430

    Add getVesselById service

--- src/components/items/Boats.js
+++ src/components/items/Boats.js
@@ -6,22 +6,31 @@ import { useSelector } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
 import { AiOutlinePlus } from "react-icons/ai";
 import AddBoat from "./addBoat";
+import axios from "axios";
 
 function Boats() {
-  const [data, setData] = useState([]);
-  const [filter, setFilter] = useState(data);
+  const [boat, setBoat] = useState([]);
+  const [filter, setFilter] = useState(boat);
   const [loading, setLoading] = useState(false);
-  const [add , setAdd] = useState(false);
+  const [add, setAdd] = useState(false);
   const userState = useSelector((userState) => userState.handleUser);
   let componentMouted = true;
+  const token = window.localStorage.getItem("AIS:ACCESS_TOKEN");
 
   useEffect(() => {
-    const getProducts = async () => {
+    const getBoats = async () => {
       setLoading(true);
-      const response = await fetch("https://fakestoreapi.com/products");
+      const response = await axios.get(
+        "http://77.237.82.37:4041/vessel/get/all",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       if (componentMouted === true) {
-        setData(await response.clone().json());
-        setFilter(await response.json());
+        setBoat(response.data.data);
+        setFilter(response.data.data);
         setLoading(false);
       }
       return () => {
@@ -29,7 +38,7 @@ function Boats() {
       };
     };
 
-    getProducts();
+    getBoats();
   }, []);
 
   const Loading = () => {
@@ -52,7 +61,7 @@ function Boats() {
   };
 
   const filterProduct = (cat) => {
-    const updatedList = data.filter((x) => x.category === cat);
+    const updatedList = boat.filter((x) => x.vesselType === cat);
     setFilter(updatedList);
   };
 
@@ -72,13 +81,13 @@ function Boats() {
             <div className="col">
               <button
                 className="btn btn-outline-dark me-2 selected mb-2"
-                onClick={() => setFilter(data)}
+                onClick={() => setFilter(boat)}
               >
                 همه
               </button>
               <button
                 className="btn btn-outline-dark me-2 selected mb-2"
-                onClick={() => filterProduct("men's clothing")}
+                onClick={() => filterProduct("ship")}
               >
                 ناوبر باری
               </button>
@@ -103,23 +112,23 @@ function Boats() {
             </div>
           </div>
         </div>
-        {filter.map((product) => {
+        {filter.map((vessel) => {
           return (
-            <div key={product.id} className="col-md-3 mb-4">
-              <div className="card h-100 text-center p-4" key={product.id}>
+            <div key={vessel.vesselId} className="col-md-3 mb-4">
+              <div className="card h-100 text-center p-4" key={vessel.vesselId}>
                 <img
-                  src={product.image}
-                  alt={product.title}
+                  src="/assets/vessel.jpg"
+                  alt={vessel.vesselName}
                   height="250px"
                   className="card-img-top"
                 />
                 <div className="card-body">
                   <h5 className="card-title mb-0">
-                    {product.title.substring(0, 12)}...
+                    {vessel.vesselName.substring(0, 12)}...
                   </h5>
-                  <p className="card-text lead fw-bold">${product.price}</p>
+                  <p className="card-text lead fw-bold">{vessel.vesselSerial}</p>
                   <NavLink
-                    to={`/boats/${product.id}`}
+                    to={`/boats/${vessel.vesselId}`}
                     className="btn btn-outline-dark"
                   >
                     اطلاعات بیشتر
@@ -148,7 +157,7 @@ function Boats() {
             <button className="btn btn-dark" onClick={handleNewBoat}>
               افزودن کشتی جدید <AiOutlinePlus />
             </button>
-            {add && <AddBoat modal={add} handleClose={() => setAdd(false)}/>}
+            {add && <AddBoat modal={add} handleClose={() => setAdd(false)} />}
           </div>
         </div>
       </div>
